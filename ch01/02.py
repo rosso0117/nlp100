262975adfa6102ac2254d9d7c609f51c19d61c6e
Tomohiro Yoshida <rollwithit0117@gmail.com>
2018-04-30 21:21:44 +0900

    first commit

---
+++ ch01/02.py
@@ -0,0 +1,5 @@
+s1 = "パトカー"
+s2 = "タクシー"
+
+for a,b in zip(s1, s2):
+    print(a, b, sep='', end='')
---
+++ ch01/03.py
@@ -0,0 +1,5 @@
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+l = [len(w.strip(".")) for w in s.split(" ")]
+
+print(l)
